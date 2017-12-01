515ef094588830812142af195dd557e56aa24d6b
gemmily <gemmily.wu@ksudi.com>
2017-12-01 17:58:36 +0800

    [scroll

--- app/src/main/java/com/gemmily/testdemo/view/ScrollerLayout.java
+++ app/src/main/java/com/gemmily/testdemo/view/ScrollerLayout.java
@@ -14,7 +14,19 @@ import android.widget.Scroller;
 
 public class ScrollerLayout extends ViewGroup {
     private Scroller mScroller;
+    /**
+     * 判定为拖动的最小移动像素数
+     */
     private int mTouchSlop;
+    /**
+     * 界面可滚动的左边界
+     */
+    private int leftBorder;
+
+    /**
+     * 界面可滚动的右边界
+     */
+    private int rightBorder;
 
     public ScrollerLayout(Context context) {
         super(context);
@@ -53,8 +65,12 @@ public class ScrollerLayout extends ViewGroup {
         if (b) {
             int count = getChildCount();
             for (int j = 0; j < count; j++) {
-
+                View child = getChildAt(j);
+                child.layout(j * child.getMeasuredWidth(), 0, (j + 1) * child.getMeasuredWidth(), child.getMeasuredHeight());
             }
+            // 初始化左右边界值
+            leftBorder = getChildAt(0).getLeft();
+            rightBorder = getChildAt(getChildCount() - 1).getRight();
         }
     }
 }
